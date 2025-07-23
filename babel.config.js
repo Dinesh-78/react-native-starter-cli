07f11cf0d61e697cf1c009dc8a2879040e893314
dinesh00root <dineshreddy0906@gmail.com>
2025-07-23 20:26:52 +0530

    feat: update package.json and add react-native.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@assets': './src/assets',
+          '@constants': './src/constants',
+          '@navigation': './src/navigation',
+          '@components': './src/components',
+          '@styles': './src/styles',
+          '@services': './src/services',
+          '@store': './src/store',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
